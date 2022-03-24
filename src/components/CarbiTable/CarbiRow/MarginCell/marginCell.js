89bdb3282dd34dacf8258179344ab418c017d36c
chroaster <chroaster@pm.me>
2022-03-23 22:53:47 -0700

    omit negative sign

--- src/components/CarbiTable/CarbiRow/MarginCell/marginCell.js
+++ src/components/CarbiTable/CarbiRow/MarginCell/marginCell.js
@@ -7,11 +7,12 @@ import {
 const MarginCell = ({ sourcePrice, targetPrice }) => {
   const [s, t] = [Number.parseFloat(sourcePrice), Number.parseFloat(targetPrice)];
   const margin = (((t - s) / s) * 100).toFixed(1);
+  const marginStr = Math.abs(margin).toString();
   const parts = [
     <span className="caret unfavorable">
       <FontAwesomeIcon icon={faLongArrowAltLeft} />
     </span>,
-    <span>{margin}</span>,
+    <span>{marginStr}</span>,
     <span className="caret favorable">
       <FontAwesomeIcon icon={faLongArrowAltRight} />
     </span>,
