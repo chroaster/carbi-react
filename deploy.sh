391d0b1f21fce93a800f4148b5d0cf041eb1a3d1
chroaster <chroaster@users.noreply.github.com>
2025-11-29 18:09:41 +0900

    converted to npm

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Yarn installing..."
+yarn install
 echo "Building..."
 yarn build
 echo "rsyncing to /srv/carbi.coolbeans.lol/"
-rsync -truv --delete --delete-excluded ./build/* chroaster@173.255.246.169:/srv/carbi.coolbeans.lol/
+rsync -truv --delete --delete-excluded ./build/* tron@172.233.86.205:/srv/carbi.coolbeans.lol/
 
 echo "Finished uploading /srv/carbi.coolbeans.lol/"
